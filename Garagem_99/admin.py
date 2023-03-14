f1f8ba7f12314ccca465ae8caecb9076f161f6ca
MiguelBochiBarros <miguelbochibarro@gmail.com>
2023-03-14 15:52:13 -0300

    Commit inicial

---
+++ Garagem_99/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Marca, Categoria
+
+admin.site.register(Marca)
+admin.site.register(Categoria)
---
+++ Garagem_99/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+    nacionalidade = models.CharField(max_length=50,null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
