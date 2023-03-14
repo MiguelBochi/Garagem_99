3a021761e2a80b24cc7302858f26a49b49d8e29a
MiguelBochiBarros <miguelbochibarro@gmail.com>
2023-03-14 16:15:34 -0300

    teste marcas e embelezamento

--- Garagem_99/admin.py
+++ Garagem_99/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 from .models import Marca, Categoria
 
-admin.site.register(Marca)
 admin.site.register(Categoria)
+admin.site.register(Marca)
--- Garagem_99/models.py
+++ Garagem_99/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Marca(models.Model):
-    nome = models.CharField(max_length=50)
-    nacionalidade = models.CharField(max_length=50,null=True, blank=True)
+    nome = models.CharField(max_length=50,)
+    nacionalidade = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
-        return self.nome
+        return self.nome.upper()
 
 
 class Categoria(models.Model):
